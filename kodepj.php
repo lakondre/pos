5a559b45223b25136dc3ec3753bd3454fd5b81eb
lakondre <120620617+lakondre@users.noreply.github.com>
2024-02-25 14:06:59 +0700

    Add files via upload

---
+++ kodepj.php
@@ -0,0 +1,15 @@
+<?php
+if (!function_exists('kode_random')) {
+	function kode_random($length)
+	{
+		$data = '1234567890';
+		$string = 'PJ-';
+		for ($i=0; $i < $length ; $i++) { 
+			$pos = rand(0, strlen($data)-1);
+			$string .= $data[$pos];
+		}
+		return $string;
+	}
+}
+$kode = kode_random(10);
+?>
